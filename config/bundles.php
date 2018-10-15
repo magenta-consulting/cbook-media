0c7da5312640c831f9ff02c845a8d9c0801c0007
binhle410 <binh@sunrise.vn>
2018-10-16 00:49:40 +0700

    Stupid Classification Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -46,6 +46,8 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true, 'test' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
+    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
 
     Magenta\Bundle\CBookModelBundle\MagentaCBookModelBundle::class => ['all' => true, 'test' => true],
     Magenta\Bundle\CBookMediaApiBundle\MagentaCBookMediaApiBundle::class => ['all' => true, 'test' => true],
