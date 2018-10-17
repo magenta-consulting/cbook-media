fd82b4d6bc8727017862fe3da712c158cd2fb27b
binhle410 <binh@sunrise.vn>
2018-10-17 11:01:03 +0700

    parameters:   nelmio_cors_allow_origin: '%env(CORS_ALLOW_ORIGIN)%'
    #jms_serializer:
    #    metadata:
    #        directories:
    #            FOSUB:
    #                namespace_prefix: "FOS\\UserBundle"
    #                path: "%kernel.root_dir%/serializer/FOSUB"
    
    nelmio_api_doc: ~
    
    nelmio_cors:
      defaults:
        origin_regex: true
        allow_origin: ['*']
        # allow_origin: ['%nelmio_cors_allow_origin%']
        allow_methods: ['GET', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE']
        allow_headers: ['Content-Type', 'Authorization']
        max_age: 3600
      paths:
        '^/': ~
    
    #twig:
    #    exception_controller: 'FOS\RestBundle\Controller\ExceptionController::showAction'
    
    sensio_framework_extra:
        router:  { annotations: true }
        request: { converters: true }
    
    # Read the documentation: https://symfony.com/doc/master/bundles/FOSRestBundle/index.html
    fos_rest:
    #    param_fetcher_listener:  true
    #    allowed_methods_listener:  true
    #    routing_loader: true
    #    view:
    #        view_response_listener:  true
    #    exception:
    #        codes:
    #            App\Exception\MyException: 403
    #        messages:
    #            App\Exception\MyException: Forbidden area.
        format_listener:
            rules:
    #            - { path: ^/media-api, priorities: ['html', 'json', '*/*'], fallback_format: html, prefer_extension: true }
                - { path: "^%env(MEDIA_API_PREFIX)%", prefer_extension: true, fallback_format: json, priorities: [ json, html ] }
                - { path: ^/, priorities: ['html', 'json', '*/*'], fallback_format: html, prefer_extension: false }
    
        param_fetcher_listener: true
        body_listener:          true
    #    format_listener:        true
        view:
    #        view_response_listener: force
            view_response_listener:  true
        body_converter:
            enabled: true
            validate: true
    
    services:
      Sonata\MediaBundle\Controller\Api\MediaController:
            alias: sonata.media.controller.api.media
            public: true

--- config/bundles.php
+++ config/bundles.php
@@ -24,33 +24,16 @@ return [
     Sonata\MediaBundle\SonataMediaBundle::class => ['all' => true],
     Bean\Bundle\SonataMediaBundle\BeanSonataMediaBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    Sonata\EasyExtendsBundle\SonataEasyExtendsBundle::class => ['all' => true],
-    Sonata\DatagridBundle\SonataDatagridBundle::class => ['all' => true],
     Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Sonata\MediaBundle\SonataMediaBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-
-
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true, 'test' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
-    Sonata\CoreBundle\SonataCoreBundle::class => ['all' => true],
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
-
     Magenta\Bundle\CBookModelBundle\MagentaCBookModelBundle::class => ['all' => true, 'test' => true],
     Magenta\Bundle\CBookMediaApiBundle\MagentaCBookMediaApiBundle::class => ['all' => true, 'test' => true],
     Magenta\Bundle\CBookJWTBundle\MagentaCBookJWTBundle::class => ['all' => true, 'test' => true],
-
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
